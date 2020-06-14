88b9b26472b2c670422107edbbd043f4ba77750c
Alex <shocer1991@gmail.com>
2020-06-14 11:42:25 +0300

    complete 4 task 2 step

--- script.js
+++ script.js
@@ -45,15 +45,19 @@ function detectDayBudget() {
 
 detectDayBudget();
 
-if (appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Высокий уровень достатка");
-} else {
-    console.log("Произошла ошибка");
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("Произошла ошибка");
+    }
 }
+
+detectLevel();
 
 function checkSavings() {
     if (appData.savings == true) {
@@ -67,4 +71,4 @@ function checkSavings() {
 
 checkSavings();
 
-//console.log(appData);
+console.log(appData);
